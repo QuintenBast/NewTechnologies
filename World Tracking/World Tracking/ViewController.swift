e24c6c01a7e80c7275934d7413e9f655cac43433
QuintenBast <36536356+QuintenBast@users.noreply.github.com>
2018-10-28 20:32:56 +0100

    Add files via upload

---
+++ World Tracking/World Tracking/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Quinten Bast on 18/10/2018.
+//  Copyright © 2018 Quinten Bast. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        node.geometry = shape
+        
+//        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNTube(innerRadius: 0.2, outerRadius: 0.3, height: 0.3)
+//        node.geometry = SCNSphere(radius: 0.1)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.3, height: 0.3)
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.4)
+//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        
+        node.geometry?.firstMaterial?.specular.contents = UIColor.red
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+
+        node.position = SCNVector3(x,y,z)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
+}
+
